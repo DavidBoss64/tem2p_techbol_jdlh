e73dcf762e65535689a6b6641e01b4369fe8af0b
DavidBoss64 <josedavidlhuayhua@gmail.com>
2026-05-26 20:31:11 -0400

    C2 Modulo de productos y clientes

--- app/clientes/routes.py
+++ app/clientes/routes.py
@@ -1,6 +1,47 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from app import db
+from app.clientes.models import Cliente
 
 bp_clientes = Blueprint('bp_clientes', __name__)
+
 @bp_clientes.route('/')
 def index():
-    return "<h1>Módulo de Clientes</h1><p>En construcción...</p>"
+    clientes = Cliente.query.all()
+    return render_template('clientes/index.html', clientes=clientes)
+
+@bp_clientes.route('/crear',methods=['GET','POST'])
+def crear():
+    if request.method == "POST":
+        nombre = request.form.get('nombre')
+        telefono = request.form.get('telefono')
+
+        nuevo_cliente = Cliente(nombre=nombre,telefono=telefono)
+
+        db.session.add(nuevo_cliente)
+        db.session.commit()
+        return redirect(url_for('bp_clientes.index'))
+    
+    return render_template('clientes/crear.html')
+
+
+@bp_clientes.route('/editar/<int:id>',methods=['GET','POST'])
+def editar(id):
+    cliente = Cliente.query.get_or_404(id)
+
+    if request.method == 'POST':
+        cliente.nombre = request.form.get('nombre')
+        cliente.telefono = request.form.get('telefono')
+
+        db.session.commit()
+        return redirect(url_for('bp_clientes.index'))
+    
+    return render_template('clientes/editar.html')
+
+
+@bp_clientes.route('/eliminar/<int:id>')
+def eliminar(id):
+    cliente = Cliente.query.get_or_404(id)
+    db.session.delete(cliente)
+    db.session.commit()
+
+    return redirect(url_for('bp_clientes.index'))
